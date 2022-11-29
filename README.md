32849fa8a0dc739b146dd573d610bf9f425b179b
Geovani carlos de souza <94084031+Geovani237@users.noreply.github.com>
2022-11-28 21:45:50 -0300

    estrutura de decisão switch

--- README.md
+++ README.md
@@ -18,4 +18,88 @@
 
 ##  Estrutura de decisão
 ### IF
-- Quando você precisa que seu código tome alguma decição
+- Quando você precisa que seu código tome alguma decição baseado em uma ou mais condições.
+
+#### Sintaxe 
+```php
+if(condição){
+    resposta verdadeira
+}else{
+    resposta falsa
+}
+```
+
+#### IF Simples
+```php
+if($idade>=18 && $fumante == false){
+    echo "<p>Sua idade é $idade </p>";
+    echo "<p>Pode entrar na festa</p>";
+}
+```
+
+#### IF com ELSE
+```php
+if($idade>=18 && $fumante == false){
+    echo "<p>Sua idade é $idade </p>";
+    echo "<p>Pode entrar na festa</p>";
+}else{
+    echo "Você não pode entrar";
+}
+```
+
+#### IF com ELSE IF
+```php
+if($nota >= 8){
+    echo "<p>Aprovado</p>";
+}else if($nota >= 5){
+    echo "<p>Recuperação</p>";
+}else{
+    echo "<p>Reprovado</p>";
+}
+```
+
+### Ternário
+#### Sintaxe
+```php
+condição ? resposta verdadeira : resposta falsa
+```
+#### Exemplo
+```php
+echo ($idade>=18)? "pode entrar": "rejeitado";
+``` 
+
+### switch
+#### Sintaxe
+```php
+switch(variável){
+    case valor:
+        código
+        break;
+    case valor:
+        código
+        break;
+    default:
+        código
+        break;
+}
+```
+#### Exemplo
+```php
+switch ($cor) {
+    case 'azul':
+        echo "<p>Sua cor favorita é <b>AZUL</b></p>";
+        break;
+    
+    case 'verde':
+        echo "<p>Sua cor favorita é <b>VERDE</b></p>";
+        break;
+    
+    case 'vermelho':
+        echo "<p>Sua cor favorita é <b>VERMELHO</b></p>";
+        break;
+        
+    default:
+        echo "<p>Sua cor favorita não é verde, nem azul e nem vermelho</p>";
+        break;
+}
+```
--- index.php
+++ index.php
@@ -44,11 +44,73 @@
     echo "operadores lógicos<br>";
     $fumante = true;
     echo !$fumante;
-    
-    if($idade >= 18) {
+
+    $idade = 75;
+    if($idade>=18 && $fumante == false){
         echo "<p>Sua idade é $idade </p>";
-        echo "<p>Pode entrar na festa!</p>";
+        echo "<p>Pode entrar na festa</p>";
+    }else if($idade >= 70){
+        echo "<p>Pessoas acima de 70 anos não podem entrar na festa</p>";
+    }else{
+        echo "Você não pode entrar";
     }
+
+    $nota = 3;
+    if($nota >= 8){
+        echo "<p>Aprovado</p>";
+    }else if($nota >= 5){
+        echo "<p>Recuperação</p>";
+    }else{
+        echo "<p>Reprovado</p>";
+    }
+
+    $idade = 15;
+    echo ($idade>=18)? "pode entrar": "rejeitado";
+
+    $cor = "verde";
+    switch ($cor) {
+        case 'azul':
+            echo "<p>Sua cor favorita é <strong>AZUL</strong></p>";
+            break;
+        case 'verde':
+            echo "<p>Sua cor favorita é <strong>VERDE</strong></p>";
+            break;
+        case 'vermelho':
+            echo "<p>Sua cor favorita é <strong>VERMELHO</strong></p>";
+            break;
+        default:
+            echo "<p>Sua cor favorita não é verde, nem azul e nem vermelho</p>";
+            break;
+    }
+
+    $dia = 8;
+    switch ($dia){
+        case '1':
+            echo "Domingo";
+            break;
+        case '2':
+            echo "Segunda";
+            break;
+        case '3':
+            echo "Terça";
+            break;
+        case '4':
+            echo "Quarta";
+            break;
+        case '5':
+            echo "Quinta";
+            break;
+        case '6':
+            echo "Sexta";
+            break;
+        case '7':
+            echo "Sábado";
+            break;
+        default:
+            echo "Dia inválido";
+            break;
+    }
+    
     ?>
 </body>
 </html>
